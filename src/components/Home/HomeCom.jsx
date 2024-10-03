285963154d0adfb9dc72be0950f957c6015d22bf
Ariet <badanovariet55@gmail.com>
2024-10-03 18:55:45 +0300

    add add

--- src/components/Home/HomeCom.jsx
+++ src/components/Home/HomeCom.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Banner from "../Banner/Banner";
 import Product from "../Product/Product";
-import CheckOut from "../checkOut/checkOut";
+
 function HomeCom() {
   return (
     <div>
       <Banner />
       <Product  />
- <CheckOut/>
+
 
   
     </div>
